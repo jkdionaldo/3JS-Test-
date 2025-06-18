8192fd44b60b35fa37c682e33e4b4707a7de186c
kev <jankevin.dionaldo64@gmail.com>
2025-06-18 10:27:02 +0800

    misc: added imports and const

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import * as Three from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
